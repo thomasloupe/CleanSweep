f863e0f416f6f777bd01cc26108440914e3c4447
Thomas Loupe <acidxrain@users.noreply.github.com>
2018-10-05 00:23:25 -0500

    10/05/18
    Initial commit.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sweep it!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(33, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(179, 13);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Deletes temp files.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 196);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(452, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 218);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "CleanSweep";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string userName = Environment.UserName;
+        public string tempDirectory;
+        long tempDirLength;
+        long sizeInMegaBytes;
+        public bool isBox1Checked = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Console.WriteLine("User, " + userName + " located!");
+            tempDirectory = "C:\\Users\\" + userName + "\\AppData\\Local\\Temp\\";
+            Console.WriteLine("Temp directory located at " + tempDirectory);
+            tempDirLength = Directory.GetFiles(tempDirectory, "*", SearchOption.AllDirectories).Sum(t => (new FileInfo(t).Length));
+            sizeInMegaBytes = tempDirLength / 1024 / 1024;
+            textBox1.Text = ("Temp Directory size: " + sizeInMegaBytes + "MB");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (isBox1Checked == true)
+            {
+                DirectoryInfo di = new DirectoryInfo(tempDirectory);
+
+                foreach (FileInfo file in di.GetFiles())
+                    try
+                    {
+                        {
+                            file.Delete();
+
+                            foreach (DirectoryInfo dir in di.GetDirectories())
+                            {
+                                dir.Delete(true);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex is IOException)
+                        {
+                            //Print the exception to our status strip and continue.
+                            toolStripStatusLabel1.Text = "Error 1: IO exception.";
+                        }
+                        else if (ex is UnauthorizedAccessException)
+                        {
+                            //Print the exception to our status strip and continue.
+                            toolStripStatusLabel1.Text = "Error 2: Access denied.";
+                        }
+                    }
+                tempDirLength = Directory.GetFiles(tempDirectory, "*", SearchOption.AllDirectories).Sum(t => (new FileInfo(t).Length));
+                sizeInMegaBytes = tempDirLength / 1024 / 1024;
+                textBox1.Text = "Temp Directory size: " + sizeInMegaBytes + "MB";
+                toolStripStatusLabel1.Text = "Successfully deleted temp files!";
+            }
+            else if (isBox1Checked == false)
+            {
+                //Let the user know we did nothing because they selected no action to perform.
+                toolStripStatusLabel1.Text = "You must select an action before using the 'Sweep It' button";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            //If checked, uncheck it.
+            if (checkBox1.Checked == false)
+            {
+                checkBox1.Checked = false;
+                isBox1Checked = false;
+            }
+            //If unchecked, check it.
+            else
+            {
+                if (checkBox1.Checked == true)
+                {
+                    checkBox1.Checked = true;
+                    isBox1Checked = true;
+                }
+            }
+        }
+    }
+}
