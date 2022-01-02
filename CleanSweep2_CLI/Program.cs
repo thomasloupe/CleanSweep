896be29fbe696631619551fa75f9298f95053b5e
thomasloupe <6563450+thomasloupe@users.noreply.github.com>
2022-01-02 04:30:10 -0600

    CleanSweep2 CLI Release
    NEW - CleanSweep2 Command Line Interface.

--- CleanSweep2_CLI/Program.cs
+++ CleanSweep2_CLI/Program.cs
@@ -13,7 +13,7 @@ namespace CleanSweep2_CLI
     internal class CleanSweep2_CLI
     {
         #region Declarations
-        private const string CurrentVersion = "v2.3.0";
+        private const string CurrentVersion = "v2.3.1";
         private readonly string _userName = Environment.UserName;
         private readonly string _systemDrive = Path.GetPathRoot(Environment.SystemDirectory);
         private readonly string _windowsDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
